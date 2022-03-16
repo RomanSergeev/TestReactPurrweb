4e2aeb38a053bd3537f7f0ec5a48b151342eda2b
RomanSergeev <romannsergeev@gmail.com>
2022-03-16 17:09:58 +0600

    Remaining files

---
+++ Scripts/Animation.js
@@ -0,0 +1,90 @@
+var STANDARD_DURATION = 1000;
+
+function Animation(dur) {
+	var start;
+	var duration = dur || STANDARD_DURATION;
+	var progress;
+	var paused = false;
+	var cancelled = false;
+	this.reverseFlag = false;
+	var cycled = false;
+	var that = this;
+	
+	this.setDuration = function(dur) {
+		duration = dur;
+	}
+	this.onStart = function() {
+		//console.log(this.name + ".onStart()");
+		progress = 0;
+		this.onUpdate(this.reverseFlag ? 1 : 0);
+	}
+	this.run = function() {
+		start = new Date().valueOf();
+		paused = false;
+		cancelled = false;
+		this.onStart();
+		animate();
+	}
+	this.cancel = function() {
+		cancelled = true;
+	}
+	this.pause = function() {
+		paused = true;
+	}
+	this.isPaused = function() {
+		return paused;
+	}
+	this.resume = function() {
+		start = new Date().valueOf();
+		paused = false;
+	}
+	this.reverse = function(flag) {
+		if (arguments.length != 1)
+			flag = !this.reverseFlag;
+		flag = !!flag;
+		if (this.reverseFlag != flag) {
+			this.reverseFlag = flag;
+			progress = 1 - progress;
+		}
+	}
+	this.slow = function(factor) {
+		duration *= factor;
+	}
+	this.cycle = function(flag) {
+		if (flag === !!flag)
+			cycled = flag;
+		else
+			cycled = !cycled;
+	}
+	function animate() {
+		if (paused) {
+			requestAnimationFrame(animate);
+			return;
+		}
+		var now = new Date().valueOf();
+		progress += (now - start) / duration;
+		start = now;
+		if (progress >= 1) {
+			progress = 1;
+			that.onFinish();
+			if (cycled)
+				that.run(duration);
+			return;
+		}
+		if (!cancelled)
+			requestAnimationFrame(animate);
+		var realProgress = that.reverseFlag ? (1 - progress) : progress;
+		//console.log(that.name + ".onUpdate(" + realProgress.toFixed(2) + ")");
+		that.onUpdate(realProgress);
+	}
+}
+
+Animation.prototype.onFinish = function() {
+	//console.log(this.name + ".onFinish()");
+	this.onUpdate(this.reverseFlag ? 0 : 1);
+}
+
+Animation.prototype.onUpdate = function(progress) {
+	this.cancel();
+	throw "You should override Animation.onUpdate() method.";
+}
---
+++ Scripts/Slider.js
@@ -0,0 +1,208 @@
+/**
+ * Slider containing different HTML elements (not necessarily images).
+ * On creation, elements are hidden, and then moved into the slider one by one as they are supposed to appear.
+ * Elements moved into slider are supposed to have their offsetWidth and offsetHeight set (thus, already rendered on the page).
+ * Slider operates with only two containers that change their content repeatedly during animations.
+ * Sliding direction is optimal (sliding through least amount of elements):
+ * Example: if clicking on 8th element in 10-element list, when active element is 1st, the slider will scroll through 1 -> 10 -> 9 -> 8.
+ * Animation used is based on window.RequestAnimationFrame.
+ * Slider is tight with its contents (i.e. has minimal necessary width and height).
+ * Some customization is easily added, like custom animation duration or dimensions.
+ * Don't use the same HTML Element in two sliders!
+ */
+function createSliderFromHTMLElementArray(HTMLElementArray, HTMLElementInsertInto) {
+	// checks
+	if (!HTMLElementArray instanceof Array || HTMLElementArray.length == 0) {
+		console.warn("createSliderFromHTMLElementArray: Trying to create slider using an empty HTMLElement array, or not array. Returned with no actions.");
+		return;
+	}
+	if (!HTMLElementInsertInto instanceof HTMLElement) {
+		console.warn("createSliderFromHTMLElementArray: Trying to insert slider not into HTMLElement. Returned with no actions.");
+		return;
+	}
+	for (var i = 0; i < HTMLElementArray.length; i++) {
+		if (!HTMLElementArray[i] instanceof HTMLElement) {
+			console.warn("createSliderFromHTMLElementArray: Argument 1 HTMLElementArray[" + i + "] is not an HTMLElement. Returned with no actions.");
+			return;
+		}
+	}
+	
+	// task constraint - optional
+	const MAX_CAROUSEL_LENGTH          =  10;
+	// arbitrary minimal slider central container dimensions
+	const MIN_SLIDER_WIDTH             = 200;
+	const MIN_SLIDER_HEIGHT            = 100;
+	// dimensions of clickable elements to the left, right and bottom of central container
+	const SLIDER_BUTTON_WIDTH          =  50;
+	const SLIDER_BOTTOM_BUTTONS_HEIGHT =  30;
+	// can be passed as a parameter (for further customization)
+	const ANIMATION_DURATION           = 300;
+	
+	var HTMLElementArrayUsed = HTMLElementArray.slice(0, 10);
+	const len = HTMLElementArrayUsed.length;
+	
+	// get dimensions of central container
+	var mainContainerWidth  = Math.max(MIN_SLIDER_WIDTH , Math.max.apply(null, HTMLElementArrayUsed.map(x => x.offsetWidth )));
+	var mainContainerHeight = Math.max(MIN_SLIDER_HEIGHT, Math.max.apply(null, HTMLElementArrayUsed.map(x => x.offsetHeight)));
+	
+	// get element offsets for centering
+	var offsetsLeft = HTMLElementArrayUsed.map(x => (mainContainerWidth  - x.offsetWidth ) / 2);
+	var offsetsTop  = HTMLElementArrayUsed.map(x => (mainContainerHeight - x.offsetHeight) / 2);
+	
+	// create slider DOM elements
+	var divSlider             = document.createElement("div");
+	var divMainPanelContainer = document.createElement("div");
+	var divLeftScroller       = document.createElement("div");
+	var divRightScroller      = document.createElement("div");
+	var divCenterContainer    = document.createElement("div");
+	var divSlideLeft          = document.createElement("div");
+	var divSlideLeftData      = document.createElement("div");
+	var divSlideMiddle        = document.createElement("div");
+	var divSlideMiddleData    = document.createElement("div");
+	var divSlideRight         = document.createElement("div");
+	var divSlideRightData     = document.createElement("div");
+	var spanLeftButton        = document.createElement("span");
+	var spanRightButton       = document.createElement("span");
+	var ulBottomButtons       = document.createElement("ul");
+	
+	// create slider object with main logic
+	var slider = {
+		carousel: divCenterContainer,
+		carouselNextElement: divSlideRight,
+		containerCurrent: divSlideMiddleData,
+		containerNext: divSlideRightData,
+		bottomIndicatorsList: ulBottomButtons,
+		selectedSlide: 0,
+		isAnimated: false,
+		rightToLeftFlag: true,
+		times: 0,
+		mainAnimation: new Animation(ANIMATION_DURATION),  // see Animation.js
+		rightToLeftMultiplier: function() {
+			return this.rightToLeftFlag ? 1 : -1;
+		},
+		getNextSlideIndex: function() {
+			return (this.selectedSlide + this.rightToLeftMultiplier() + len) % len;
+		},
+		launchAnimation: function() {
+			var nextSlideIndex = this.getNextSlideIndex();
+			this.fillContainerNext(nextSlideIndex);
+			this.selectedSlide = nextSlideIndex;
+			this.mainAnimation.run();
+		},
+		onClickAction: function(event, index) {
+			if (event.which != 1 ||
+				this.selectedSlide == index ||
+				this.isAnimated) return;
+			this.slideTo(index);
+		},
+		updateBottomIndicator: function(index) {
+			this.bottomIndicatorsList.children[this.selectedSlide].classList.remove("selected");
+			this.bottomIndicatorsList.children[index].classList.add("selected");
+		},
+		slideTo: function(index) {
+			this.isAnimated = true;  // lock
+			var diff = index - this.selectedSlide;
+			this.rightToLeftFlag = (diff + len) % len <= len / 2;
+			this.updateBottomIndicator(index);
+			var numAnimations = Math.min((diff + len) % len, (-diff + len) % len);
+			this.mainAnimation.setDuration(ANIMATION_DURATION / numAnimations);
+			this.times = numAnimations;
+			this.launchAnimation();
+		},
+		fillContainerCurrent: function(index) {
+			this.containerCurrent.style.left = offsetsLeft[index] + "px";
+			this.containerCurrent.style.top = offsetsTop[index] + "px";
+			HTMLElementArrayUsed[index].style.display = "block";
+			if (this.containerCurrent.firstChild) {
+				this.containerCurrent.removeChild(this.containerCurrent.firstChild);
+			}
+			this.containerCurrent.appendChild(HTMLElementArrayUsed[index]);
+		},
+		fillContainerNext: function(index) {
+			this.containerNext.style.left = offsetsLeft[index] + "px";
+			this.containerNext.style.top = offsetsTop[index] + "px";
+			HTMLElementArrayUsed[index].style.display = "block";
+			if (this.containerNext.firstChild) {
+				this.containerNext.removeChild(this.containerNext.firstChild);
+			}
+			this.carouselNextElement.style.left = (this.rightToLeftFlag ? "" : "-") + "100%";
+			this.containerNext.appendChild(HTMLElementArrayUsed[index]);
+		}
+	}
+	slider.mainAnimation.onUpdate = progress => slider.carousel.style.left = -slider.rightToLeftMultiplier() * progress * mainContainerWidth + "px";
+	slider.mainAnimation.onFinish = function() {
+		slider.carousel.style.left = 0;  // default action is onUpdate(1)
+		slider.fillContainerCurrent(slider.selectedSlide);
+		slider.times--;
+		if (slider.times > 0) {
+			slider.launchAnimation();
+		} else {
+			slider.isAnimated = false;
+		}
+	}
+	
+	// add event listeners
+	for (var i = 0; i < len; i++) {
+		var liBottomButton = document.createElement("li");
+		liBottomButton.classList.add("sliderBottomButton");
+		liBottomButton.textContent = "•";
+		(function(j) {
+			liBottomButton.addEventListener("click", event => slider.onClickAction(event, j));
+		})(i);
+		ulBottomButtons.appendChild(liBottomButton);
+	}
+	divLeftScroller .addEventListener("click", event => slider.onClickAction(event, (slider.selectedSlide + len - 1) % len));
+	divRightScroller.addEventListener("click", event => slider.onClickAction(event, (slider.selectedSlide + 1) % len));
+	
+	divSlider.style.width = mainContainerWidth + 2 * SLIDER_BUTTON_WIDTH + "px";
+	divSlider.style.height = mainContainerHeight + SLIDER_BOTTOM_BUTTONS_HEIGHT + "px";
+	
+	// add classnames
+	divLeftScroller      .classList.add("sliderScroller");
+	divRightScroller     .classList.add("sliderScroller");
+	divSlideMiddle       .classList.add("slide");
+	divSlideRight        .classList.add("slide");
+	divSlideMiddleData   .classList.add("slideData");
+	divSlideRightData    .classList.add("slideData");
+	divCenterContainer   .classList.add("sliderCenterContainer");
+	divMainPanelContainer.classList.add("sliderMainPanelContainer");
+	spanLeftButton       .classList.add("sliderScrollerButton", "buttonLeft");
+	spanRightButton      .classList.add("sliderScrollerButton", "buttonRight");
+	ulBottomButtons      .classList.add("sliderBottomButtons");
+	divSlider            .classList.add("slider");
+	
+	// append elements
+	divLeftScroller      .appendChild(spanLeftButton);
+	divRightScroller     .appendChild(spanRightButton);
+	divSlideMiddle       .appendChild(divSlideMiddleData);
+	divSlideRight        .appendChild(divSlideRightData);
+	divCenterContainer   .appendChild(divSlideMiddle);
+	divCenterContainer   .appendChild(divSlideRight);
+	divMainPanelContainer.appendChild(divLeftScroller);
+	divMainPanelContainer.appendChild(divCenterContainer);
+	divMainPanelContainer.appendChild(divRightScroller);
+	divSlider            .appendChild(divMainPanelContainer);
+	divSlider            .appendChild(ulBottomButtons);
+	HTMLElementInsertInto.appendChild(divSlider);
+	
+	// hide added elements
+	HTMLElementArrayUsed.forEach(element => element.style.display = "none");
+	
+	// fill the two elements with initial values
+	slider.fillContainerCurrent(0);
+	slider.updateBottomIndicator(0);
+	return slider;
+}
+
+function $(id) {
+	return document.getElementById(id);
+}
+
+window.onload = function() {
+	var body = document.body;
+	var elems1 = [$("img0"), $("img1"), $("p2"), $("ul3"), $("img4"), $("img5"), $("img6"), $("img7"), $("img8"), $("div9"), $("eleventh")];
+	var elems2 = [$("loneImage")];
+	var slider1 = createSliderFromHTMLElementArray([], body);
+	var slider3 = createSliderFromHTMLElementArray(elems1, body);
+	var slider2 = createSliderFromHTMLElementArray(elems2, body);
+}
